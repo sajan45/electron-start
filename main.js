d4cec2e339339c2f1090dc8a01af7265334af823
sajan <sajan.sahu@live.com>
2019-01-02 21:18:17 +0530

    Stable version with logging and auto update
    removed debugging lines

--- main.js
+++ main.js
@@ -301,27 +301,27 @@ app.on('activate', function () {
 })
 app.on('before-quit', () => willQuitApp = true);
 
-autoUpdater.on('checking-for-update', () => {
-  log.info('Checking for update...');
-})
-autoUpdater.on('update-available', (info) => {
-  log.info('Update available.');
-})
-autoUpdater.on('update-not-available', (info) => {
-  log.info('Update not available.');
-})
-autoUpdater.on('error', (err) => {
-  log.info('Error in auto-updater. ' + err);
-})
-autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-  log.info(log_message);
-})
-autoUpdater.on('update-downloaded', (info) => {
-  log.info('Update downloaded');
-});
+// autoUpdater.on('checking-for-update', () => {
+//   log.info('Checking for update...');
+// })
+// autoUpdater.on('update-available', (info) => {
+//   log.info('Update available.');
+// })
+// autoUpdater.on('update-not-available', (info) => {
+//   log.info('Update not available.');
+// })
+// autoUpdater.on('error', (err) => {
+//   log.info('Error in auto-updater. ' + err);
+// })
+// autoUpdater.on('download-progress', (progressObj) => {
+//   let log_message = "Download speed: " + progressObj.bytesPerSecond;
+//   log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+//   log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+//   log.info(log_message);
+// })
+// autoUpdater.on('update-downloaded', (info) => {
+//   log.info('Update downloaded');
+// });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
